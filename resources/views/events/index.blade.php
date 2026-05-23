78fbfa719b6f4783381716e00f530ea57706a2cd
mikuh21 <mikamsanchez21@gmail.com>
2026-05-23 19:05:51 +0800

    Fix edit event dropdown arrow design

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -565,6 +565,16 @@
             transition: border-color 160ms ease, box-shadow 160ms ease;
         }
 
+        .edit-event-form select {
+            appearance: none;
+            -webkit-appearance: none;
+            -moz-appearance: none;
+            background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='%235BA4CF' stroke-width='2.2' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpath d='m6 9 6 6 6-6'/%3E%3C/svg%3E");
+            background-repeat: no-repeat;
+            background-position: right 12px center;
+            background-size: 14px 14px;
+        }
+
         .edit-event-form input:focus,
         .edit-event-form select:focus,
         .edit-event-form textarea:focus {
