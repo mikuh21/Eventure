2b39bb4664418cb089db9c4e3cd47b83c957add7
mikuh21 <mikamsanchez21@gmail.com>
2026-05-23 19:00:58 +0800

    Fix participant type dropdown arrow design

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -351,6 +351,16 @@
             transition: border-color 180ms ease, box-shadow 180ms ease;
         }
 
+        .landing-registration-field select {
+            appearance: none;
+            -webkit-appearance: none;
+            -moz-appearance: none;
+            background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='%235BA4CF' stroke-width='2.2' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpath d='m6 9 6 6 6-6'/%3E%3C/svg%3E");
+            background-repeat: no-repeat;
+            background-position: right 12px center;
+            background-size: 14px 14px;
+        }
+
         .landing-registration-field input[type="file"] {
             min-height: 40px;
             padding: 4px 8px;
