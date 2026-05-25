f9a95107fdb536152763219d17e198b0b5c0869b
mikuh21 <mikamsanchez21@gmail.com>
2026-05-25 18:30:23 +0800

    Fix not showing file in Submission column in Guests page

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -157,6 +157,12 @@ class GuestController extends Controller
         // Directly added guests by admin/event staff are approved immediately
         $validated['status'] = 'approved';
 
+        if ($request->hasFile('conference_paper') && $request->file('conference_paper')->isValid()) {
+            $validated['conference_paper_original_name'] = $request->file('conference_paper')->getClientOriginalName();
+            $validated['conference_paper_path'] = $request->file('conference_paper')
+                ->store('conference_papers', 'event-templates');
+        }
+
         $guest = Guest::create($validated);
 
         if (! $guest->digital_token) {
