8e863061b0cb4faa2cb291aafdab5a6292bfadc1
mikuh21 <mikamsanchez21@gmail.com>
2026-06-06 03:15:53 +0800

    Fix Eventure label logo in sign in page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -498,7 +498,7 @@
     <nav class="top-nav">
         <a href="{{ route('landing', [], false) }}" class="brand" aria-label="Eventure home">
             <img src="{{ asset('eventure-signinlogo.png') }}" alt="Eventure logo">
-            <span class="brand-text"><span class="brand-event">Even</span><span class="brand-flow">ture</span></span>
+            <span class="brand-text"><span class="brand-event">Event</span><span class="brand-flow">ure</span></span>
         </a>
     </nav>
 
