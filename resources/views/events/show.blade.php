3206ba1c77f9544ba6e74bcdaa5d28e0d00e1256
mikuh21 <mikamsanchez21@gmail.com>
2026-06-05 22:16:34 +0800

    Fix Program preview and download

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -597,7 +597,7 @@
                         </iframe>
                     </div>
                     <div class="event-asset-actions">
-                        <a class="btn" href="{{ route('events.download-program-public', $event) }}" style="font-family: 'Sora', sans-serif;">Download Program</a>
+                        <a class="btn" href="/events/{{ $event->id }}/program/download" style="font-family: 'Sora', sans-serif;">Download Program</a>
                     </div>
                 </div>
             @endif
