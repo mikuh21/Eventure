95d8f0ac6709c5961951f786388c71c5c22e86cf
mikuh21 <mikamsanchez21@gmail.com>
2026-06-06 03:40:39 +0800

    Fix Eventure label logo in side bar nav of Analytics

--- resources/views/admin/event-analytics.blade.php
+++ resources/views/admin/event-analytics.blade.php
@@ -337,7 +337,7 @@
         <aside class="sidebar" id="analyticsSidebar">
             <a href="{{ \App\Support\PreviewAuth::appendToUrl(route('admin.dashboard', [], false), $previewAuthQuery) }}" class="brand">
                 <img src="{{ asset('eventurelogo.png') }}" alt="Eventure logo">
-                <span class="brand-text"><span class="brand-event">Even</span><span class="brand-flow">ture</span></span>
+                <span class="brand-text"><span class="brand-event">Event</span><span class="brand-flow">ure</span></span>
             </a>
 
             <nav class="side-nav" aria-label="Main navigation">
